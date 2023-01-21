8b6f3208e658b061b61e86940f155432f90c2c3c
Remi <damengwu@outlook.com>
2023-01-21 21:49:44 +0100

    Added bluetooth action listener for ECG signal transfer

--- NaviDrawer/app/src/main/java/com/remi/navidrawer/ui/ecg/EcgFragment.java
+++ NaviDrawer/app/src/main/java/com/remi/navidrawer/ui/ecg/EcgFragment.java
@@ -1,8 +1,15 @@
 package com.remi.navidrawer.ui.ecg;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.Manifest;
+import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,8 +21,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -27,66 +37,47 @@ import java.util.Set;
 public class EcgFragment extends Fragment {
 
     private FragmentEcgBinding binding;
-    private BroadcastReceiver mReceiver;
     private IntentFilter mIntentFilter;
+    private int REQUEST_ENABLE_BT = 1;
+    private int BLUETOOTH_ENABLE_SUCCESS = -1;
+    private EcgViewModel ecgViewModel;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                switch (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)) {
+                    case BluetoothAdapter.STATE_ON:
+                        Toast.makeText(getContext(), "BluetoothAdapter.STATE_ON", Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothAdapter.STATE_OFF:
+                        Toast.makeText(getContext(), "BluetoothAdapter.STATE_OFF", Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Toast.makeText(getContext(), "BluetoothAdapter.STATE_TURNING_ON", Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Toast.makeText(getContext(), "BluetoothAdapter.STATE_TURNING_OFF", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                }
+            }
+        }
+    };
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         Log.d("I am inside", "Here");
-        EcgViewModel ecgViewModel =
+        ecgViewModel =
                 new ViewModelProvider(this).get(EcgViewModel.class);
         binding = FragmentEcgBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         final TextView textView = binding.textEcg;
         ecgViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            ecgViewModel.setText("There is no Bluetooth adapter on your device");
-        } else {
-            ecgViewModel.setText("Bluetooth adapter is available!");
-        }
-
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBtIntent);
-        }
-
-
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-        }
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-
-        if (pairedDevices.size() > 0) {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                Log.d("Bluetooth device:",deviceName);
-                Log.d("deviceHardwareAddress:",deviceHardwareAddress);
-            }
-        }
-
-        mIntentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        mReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Log.d("BLUETOOTH CHECK"," onRecieve"); //do something with intent
-            }
-        };
-
-        int requestCode = 1;
-        Intent discoverableIntent =
-                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-        startActivityForResult(discoverableIntent, requestCode);
+        initBluetooth();
+        scanBluetoothDevice();
         return root;
     }
 
@@ -94,12 +85,71 @@ public class EcgFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        getActivity().unregisterReceiver(mReceiver);
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         getActivity().registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    public void initBluetooth() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            ecgViewModel.setText("There is no Bluetooth adapter on your device");
+        } else {
+            ecgViewModel.setText("Bluetooth adapter is available!");
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    getActivity(),
+                    new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1
+            );
+        }
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    }
+
+    public void scanBluetoothDevice() {
+
+        mIntentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        mIntentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        getActivity().registerReceiver(mReceiver, mIntentFilter);
+        Intent discoverableIntent =
+                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1: {
+                if (resultCode == BLUETOOTH_ENABLE_SUCCESS) {
+                    Toast.makeText(getContext(), "Bluetooth activated", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), "Activation deined", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case 2:
+                break;
+            default:
+        }
     }
 
 }
